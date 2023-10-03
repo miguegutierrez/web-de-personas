52673c81ed2013b6c29358460bc15c8820f0c0b3
miguegutierrez <miguelonmelon92@gmali.com>
2023-10-03 18:07:21 -0300

    buscador/filtro en tiempo real

--- index.js
+++ index.js
@@ -2,28 +2,85 @@
 
 
 
-const checkSwitch = document.getElementById("flexSwitchCheckDefault");
-const configUser = window.matchMedia(`(prefers-color-scheme: dark)`);
+
+const configUser  = window.matchMedia(`(prefers-color-scheme: dark)`);//configuracion tema claro/oscuro del sistema
+const localConfig  = localStorage.getItem('tema'); //key localStorage para confiuraicon de tema
+const themeSwitch = document.getElementById("themeSwitch");//invoca el  checkbox/switch
+
+//cambia  la clase del body segun configuracion de tema claro/oscuro definido en el sistema Operativo
+if (localConfig ==='dark'){
+  document.body.classList.toggle('dark-theme')
+}else if (localConfig ==='light'){
+  document.body.classList.toggle('light-theme')
+}
+
 
 //Agrega un evento click al boton chackSwitch
-checkSwitch.addEventListener(`click`, () => {
-       
+themeSwitch.addEventListener(`click`, () => {
+    let colorTheme ;
+    
     if (configUser.matches===false) {
         
-        document.body.classList.toggle('dark-theme')
-       } else{
-        document.body.classList.toggle('light-theme')
-
-       }
+      colorTheme = document.body.classList.contains('dark-theme') ? 'light' : 'dark' //colorTheme será el value para el local storage 
+       
+      document.body.classList.toggle('dark-theme')
+       
+     } else {
+         document.body.classList.toggle('light-theme')
+        }
+       localStorage.setItem('tema' , colorTheme)
   });
+  
 
+  async function fetchData(){
+    try{ 
+        const response = await fetch('http://localhost:3000/personas');
+        const data = await response.json();
+        const $fragment = document.createDocumentFragment();
+        const listaDePersonas = document.getElementById("listaDePersonas"); 
+        console.log(response,data);
+        
+        //se manda al catch  el estado de respuesta (status)  y el mensaje dedicho error (statusText)
+       if(!response.ok) throw{status:response.status, statusText:response.statusText}
+        
+        //creamos un "span" para cada elemento con la información correspondiente  de cada elemento 
+        data.forEach((e) => {
+          const $span = document.createElement("span");
+          $span.innerHTML = 
+            `
+            <h3>${e.name}</h3><br>
+            <p>Telefono: ${e.phone}<p>
+            <p>País: ${e.country}<p>
+            `;
 
+          $fragment.appendChild($span);
+          
+          listaDePersonas.appendChild($fragment);
+        });
 
-/*fetch('https://api.npoint.io/103b89cbfa324e55fd14')
-  .then(response => response.json()) // Parsear respuesta JSON
-  .then(data => {
-    // Trabajar con los datos aquí
-  })
-  .catch(error => {
-    // Manejar errores aquí
-  });*/
+      }catch(error){
+        //manejar error aqui
+        console.log(error);
+        let message = error.statusText = "ocurrió un error";
+        listaDePersonas.innerHTML = `Error ${error.status}:<br> ${message}`;
+      }
+      
+    }
+        // buscdor/filtro  en tiempo real
+        //agregamos  evento k
+    document.addEventListener("keyup", e =>{
+      if (e.target.matches("#searchPerson")){
+         
+        if(e.key==="Escape")e.target.value = ""
+
+        document.querySelectorAll("span").forEach(persona => {
+          persona.textContent.toLocaleLowerCase().includes(e.target.value.toLocaleLowerCase())
+          ?persona.classList.remove("filtro")
+          :persona.classList.add("filtro")
+        })
+      }
+    })
+    
+    fetchData();
+    
+   
